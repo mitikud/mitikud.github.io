10105e0b701e350a0947a6c8dbfc53e863354419
mitikud <mitikud@gmail.com>
2021-05-23 21:38:29 -0500

    checkBox

--- decoratemytext/decoratemytext.js
+++ decoratemytext/decoratemytext.js
@@ -1,3 +1,25 @@
-function callAlert(){
-    document.getElementById("txtDecoration").style.fontSize="24pt"
+function callDecoration(){
+    //var txtDeco = document.getElementById("txtDecoration");
+    //txtDeco.style.fontSize="24pt";
+    //document.getElementById("txtDecoration").style.fontSize = parseInt(document.getElementById("txtDecoration").style.fontSize)+ 2 + "em";
+    document.getElementById("txtDecoration").style.fontSize = parseInt(document.getElementById("txtDecoration").style.fontSize) + 2 + "pt";
+
+}   
+
+timer = null;
+function keepDecoration() {
+    if(timer == null) {
+        timer = setInterval(callDecoration, 500);
+    }
+    else{
+        clearInterval(timer);
+        timer = null;
+    }
+}
+
+function onChange(){
+    
+    //alert("checkbox is checked");
+    var txtDeco = document.getElementById("txtDecoration");
+    txtDeco.style.fontWeight = "bold";
 }
