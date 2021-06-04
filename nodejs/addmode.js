4acb39df1f24933f7ecc718dc09afe9b5013f091
mitikud <mitikud@gmail.com>
2021-06-04 08:28:59 -0500

    update

--- nodejs/addmode.js
+++ nodejs/addmode.js
@@ -1,24 +1,8 @@
 exports.add = function (req,res,vals) {
-    var operator = "";
-    var result = 0;
-  /*   $('select').on('change', function() {
-        operator = this.value ;
-    });  */
-     operator =vals.operator;
 
-    if(operator == "+") {
-        result = parseInt(vals.first) + parseInt(vals.second);
-    }
-    else if(operator == "-") {
-        result = parseInt(vals.first) - parseInt(vals.second);
-    }
-    else if(operator == "x") {
-        result = parseInt(vals.first) * parseInt(vals.second);
-    }
-    else{
-        result = parseInt(vals.first) / parseInt(vals.second);
-        
-    }
+   var result = parseInt(vals.first) + parseInt(vals.second);
+
+    
     
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("<!DOCTYPE html>");
@@ -30,7 +14,7 @@ exports.add = function (req,res,vals) {
     res.write("<p>The sum is: ");
     res.write(String(result) + "<br>");
     res.write("</p>");
-    res.write("<p><a href = 'index.html'>Another Calculation</a></p>")
+    res.write("<p><a href = 'index.html'>Back to Home</a></p>")
     res.write("</body>");
     res.write("</html>");
     return res.end();
