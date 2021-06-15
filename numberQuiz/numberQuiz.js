e760f6f1dbe483df0d066c1988ca5b5a7d77b739
mitikud <mitikud@gmail.com>
2021-06-15 03:28:43 -0500

    number quize

--- numberQuiz/numberQuiz.js
+++ numberQuiz/numberQuiz.js
@@ -82,7 +82,7 @@ function displayQuestion(req, res, list, score, hidden) {
   res.write("<title>Quiz Game</title>");
   res.write("</head>");
   res.write("<body>");
-  res.write('<form action="http://localhost:8080/">');
+  res.write('<form action="http://localhost:8085/">');
   res.write("<label name=quiznumber>");
   res.write(`<input type="hidden" name="quiznumber" value=${hidden} />`);
   res.write("<div>");
--- numberQuiz/numberQuizserver.js
+++ numberQuiz/numberQuizserver.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 var output = require("./numberQuiz.js");
 http
   .createServer(function (req, res) {
-    var q = new URL(req.url, "https://localhost:8080/");
+    var q = new URL(req.url, "https://localhost:8085/");
     output.guessnumber(req, res, q.searchParams);
   })
   .listen(8085);
